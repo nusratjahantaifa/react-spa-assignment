678b6a663090ae2f0d1c68c2b0ab47e88d615e04
nusratjahantaifa <nusratjahantaifa05@gmail.com>
2025-12-16 20:47:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+     <div className='flex gap-6 ml-4 w-full m-4'>
+      
+      <nav className='flex gap-8'>
+        <h2 className='text-2xl '>Recipe Calories</h2>
+       
+       
+      <ul className='text-blue-600 flex gap-8'>
+        <li>Home</li>
+        <li>Recipe</li>
+        <li>About</li>
+        <li>Search</li>
+      </ul>
+      <button className="btn btn-wide text-grey-600 p-2 bg-gray-400">search</button>
+      </nav> 
+     </div>
+     
+    </>
+  )
+}
+
+export default App
